f6df840c7e18cb759107157148a9962b93426d84
Maxime Bourdin <maxime.bourdin.tic@gmail.com>
2022-11-26 17:39:56 +0100

    code Maxime homework

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,31 +1,38 @@
-import './App.css';
+import "./App.css";
 import { Container, Row, Col, Card } from "react-bootstrap";
-import { useEffect, useState, createContext } from 'react';
-import { services } from '.';
-import FormFilter from './components/FormFilter';
-import PatientCard from './components/PatientCard';
+import { useEffect, useState, createContext } from "react";
+import { services } from ".";
+import FormFilter from "./components/FormFilter";
+import PatientCard from "./components/PatientCard";
 
 export interface ICard {
-  status: string,
-  display: boolean,
-  id: number,
-  patient_name: string,
-  created_date: string,
-  arrhythmias:string[]
+  status: string;
+  display: boolean;
+  id: number;
+  patient_name: string;
+  created_date: string;
+  arrhythmias: string[];
 }
 export type AppContent = {
-  cardsDisplayed: ICard[], setCardsDisplayed:(c:ICard[]) => void
-}
-export const ContextApp = createContext<AppContent>({cardsDisplayed:[], setCardsDisplayed:() => {}});
+  cardsDisplayed: ICard[];
+  setCardsDisplayed: (c: ICard[]) => void;
+};
+export const ContextApp = createContext<AppContent>({
+  cardsDisplayed: [],
+  setCardsDisplayed: () => {},
+});
 
 function App() {
   const [cardsDisplayed, setCardsDisplayed] = useState<ICard[]>([]);
 
+  const listStatus: string[] = ["PENDING", "REJECTED", "DONE"];
+
   useEffect(() => {
-    services.app.getAllCards()
-      .then(resp => resp.json())
-      .then(data => {
-        data.forEach((card: any) => card.display = true);
+    services.app
+      .getAllCards()
+      .then((resp) => resp.json())
+      .then((data) => {
+        data.forEach((card: any) => (card.display = true));
         setCardsDisplayed(data);
       });
   }, []);
@@ -40,68 +47,32 @@ function App() {
         </header>
         <Container>
           <Row>
-            <Col>
-              <Card className="container-patients-card">
-                <Card.Header as="h2">PENDING</Card.Header>
-                <Card.Body>
-                  {cardsDisplayed.map((card, i) => {
-                    switch (card.status) {
-                      case "PENDING":
-                        if (card.display === true) {
-                          return <PatientCard key={`card-id-${i}`} card={card} />
-                        } else {
-                          return <></>
+            {listStatus.map((status, i) => {
+              return (
+                <Col>
+                  <Card className="container-patients-card">
+                    <Card.Header as="h2">{status}</Card.Header>
+                    <Card.Body>
+                      {cardsDisplayed.map((card, i) => {
+                        switch (card.status) {
+                          case status:
+                            if (card.display === true) {
+                              return (
+                                <PatientCard key={`card-id-${i}`} card={card} />
+                              );
+                            } else {
+                              return <></>;
+                            }
+
+                          default:
+                            return <></>;
                         }
-
-                      default:
-                        return <></>;
-                    }
-                  })}
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card className="container-patients-card">
-                <Card.Header as="h2">REJECTED</Card.Header>
-                <Card.Body>
-                  {cardsDisplayed.map((card, i) => {
-                    switch (card.status) {
-                      case "REJECTED":
-                        if (card.display === true) {
-                          return <PatientCard key={`card-id-${i}`} card={card} />
-                        } else {
-                          return <></>
-                        }
-
-                      default:
-                        return <></>;
-                    }
-                  })}
-
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card className="container-patients-card">
-                <Card.Header as="h2">DONE</Card.Header>
-                <Card.Body>
-                  {cardsDisplayed.map((card, i) => {
-                    switch (card.status) {
-                      case "DONE":
-                        if (card.display === true) {
-                          return <PatientCard key={`card-id-${i}`} card={card} />
-                        } else {
-                          return <></>
-                        }
-
-                      default:
-                        return <></>;
-                    }
-                  })}
-
-                </Card.Body>
-              </Card>
-            </Col>
+                      })}
+                    </Card.Body>
+                  </Card>
+                </Col>
+              )
+            })}
           </Row>
         </Container>
       </div>
